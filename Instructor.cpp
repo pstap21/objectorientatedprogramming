2bf46278dfd4e35cc357dce7fbe1e250e53053e5
pstap21 <a1629853@adelaide.edu.au>
2024-09-11 17:21:57 +0930

    Add files via upload

---
+++ Instructor.cpp
@@ -0,0 +1,4 @@
+
+#include "Instructor.h"
+
+Instructor::Instructor(std::string name, int id) : Person(name, id) {}
---
+++ Instructor.h
@@ -0,0 +1,12 @@
+
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+
+#include "Person.h"
+
+class Instructor : public Person {
+public:
+    Instructor(std::string name, int id);
+};
+
+#endif
